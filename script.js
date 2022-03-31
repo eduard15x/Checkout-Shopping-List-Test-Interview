935a3f210c456ec8877e95bace39971ce767af05
Precup Eduard <precupeduard99@gmail.com>
2022-03-31 18:45:12 +0300

    Creating the code

--- script.js
+++ script.js
@@ -34,7 +34,6 @@ const arraySortByPrice = productsArray.sort((a,b)=>b.price-a.price)
 console.log(arraySortByPrice)
 
 
-
 const shoppingProductsCol = document.querySelector('.shopping-products-div')
 
 // Create the list products
@@ -46,44 +45,147 @@ shoppingProductsCol.append(productsList);
 let sumTotalPrice = 0;
 
 
+let arrayProductsAdded = [];
 
 
 
 
-
-
-
-
-// Create the shopping cart with products added
 const shoppingCart = document.querySelector('.shopping-cart');
+shoppingCart.innerHTML = `<p id="pentry"></p>`
+let pentry = document.getElementById('pentry')
+pentry.textContent = 'No products in your shopping list'
 
 const pTitle = document.createElement('p');
-pTitle.textContent = 'No products in shopping cart';
-
 const productsSubtitle = document.createElement('div');
 productsSubtitle.classList.add('products-added-description');
-productsSubtitle.innerHTML = 
-`
-<p class="product-added-name">Product</p>
-<p class="product-added-quantity">Quantity</p>
-<p class="product-added-price">Value</p>
-`
-
 
 const hr = document.createElement('hr');
-hr.id= 'product-border'
+hr.id = 'product-border'
 
 const productsCartList = document.createElement('ul');
 productsCartList.classList.add('products-from-cart');
 
 const totalPrice = document.createElement('p');
 totalPrice.id = 'total-price'
-totalPrice.textContent = `Total price: $${sumTotalPrice}`;
 
 const buttonBuy = document.createElement('button')
 buttonBuy.id ='button-buy';
-buttonBuy.textContent = 'Continue';
-shoppingCart.append(pTitle, productsSubtitle, hr, productsCartList, totalPrice, buttonBuy)
+
+
+
+
+
+
+
+// const btnAddProduct = document.querySelectorAll('.button-add-product');
+
+// for ( let i = 0; i < arraySortByPrice.length; i++) {
+//     btnAddProduct[i].addEventListener('click', () => {
+//         if( arrayProductsAdded.includes(arraySortByPrice[i])) {
+//             return;
+//         } else {
+//             arrayProductsAdded.push(arraySortByPrice[i])
+        
+//             shoppingCart.innerHTML = ''
+//             // Create the shopping cart with products added
+//             pTitle.textContent = 'Products in your shopping cart';
+
+//             productsSubtitle.innerHTML = 
+//             `
+//             <p class="product-added-name">Product</p>
+//             <p class="product-added-quantity">Quantity</p>
+//             <p class="product-added-price">Value</p>
+//             `
+//             const productLiAdded = document.createElement('li');
+//             productLiAdded.classList.add('lili')
+//             productsCartList.append(productLiAdded);
+
+//             productLiAdded.innerHTML = 
+//             `
+//             <p class="product-added-name">${arraySortByPrice[i].name}<span class="product-p-description" title="${arraySortByPrice[i].description}">ℹ</span></p>
+//             <input type="number" class="product-added-quantity" value="1">
+//             <p class="product-added-price">$${arraySortByPrice[i].price}</p>
+//             <p class="remove-product">X</p>
+//             `   
+//             sumTotalPrice += arraySortByPrice[i].price
+//             totalPrice.textContent = `Total price: $${sumTotalPrice.toFixed(2)}`;
+//             buttonBuy.textContent = 'Continue';
+//             shoppingCart.append(pTitle, productsSubtitle, hr, productsCartList, totalPrice, buttonBuy)
+//         }
+//         const btnRemoveElement = document.querySelectorAll('.remove-product')
+//         for ( let i = 0; i < arrayProductsAdded.length; i++) {
+//             btnRemoveElement[i].addEventListener('click', ()=> {
+//                 btnRemoveElement[i].parentElement.remove()
+//                 arrayProductsAdded.splice([i], 1)
+//             })
+//         }
+       
+//     })
+    
+// }
+
+// Create the li - items
+for ( let i = 0; i < arraySortByPrice.length; i++) {
+    const productLi = document.createElement('li')
+    productLi.innerHTML = 
+    `
+    <p class="product-p">${arraySortByPrice[i].name}<span title="${arraySortByPrice[i].description}" class="product-p-description">ℹ</span></p>
+    <p class="price-p">Price: <span class="price-p-span">$${arraySortByPrice[i].price}</span></p>
+    <button class="button-add-product">Add to cart</button>
+    `
+    productsList.append(productLi)
+
+}
+const btnAddProduct = document.querySelectorAll('.button-add-product');
+
+
+
+
+
+
+
+for ( let i = 0; i < arraySortByPrice.length; i++) {
+    btnAddProduct[i].addEventListener('click', () => {
+        if( arrayProductsAdded.includes(arraySortByPrice[i])) {
+            return;
+        } else {
+            arrayProductsAdded.push(arraySortByPrice[i])
+        
+            pentry.textContent = "Hey, this are your products"
+            // Create the shopping cart with products added
+            pTitle.textContent = 'Products in your shopping cart';
+            productsSubtitle.innerHTML = 
+            `
+            <p class="product-added-name">Product</p>
+            <p class="product-added-quantity">Quantity</p>
+            <p class="product-added-price">Value</p>
+            `
+            const productLiAdded = document.createElement('li');
+            productLiAdded.classList.add('lili')
+            productsCartList.append(productLiAdded);
+            productLiAdded.innerHTML = 
+            `
+            <p class="product-added-name">${arraySortByPrice[i].name}<span class="product-p-description" title="${arraySortByPrice[i].description}">ℹ</span></p>
+            <input type="number" class="product-added-quantity" value="1">
+            <p class="product-added-price">$${arraySortByPrice[i].price}</p>
+            <p class="remove-product">X</p>
+            `   
+            sumTotalPrice += arraySortByPrice[i].price
+            totalPrice.textContent = `Total price: $${sumTotalPrice.toFixed(2)}`;
+            buttonBuy.textContent = 'Continue';
+            shoppingCart.append(pTitle, productsSubtitle, hr, productsCartList, totalPrice, buttonBuy)
+        }
+        const btnRemoveElement = document.querySelectorAll('.remove-product')
+        for ( let i = 0; i < arrayProductsAdded.length; i++) {
+            btnRemoveElement[i].addEventListener('click', ()=> {
+                btnRemoveElement[i].parentElement.remove()
+                arraySortByPrice.splice(i, 1)
+            })
+        }
+    })
+}
+
+
 
 
 
@@ -114,67 +216,51 @@ shoppingCart.append(pTitle, productsSubtitle, hr, productsCartList, totalPrice, 
 
 
 
-let arrayH = [];
-// Create the li - items
-function createLi(array) {
-    for ( let i = 0; i < array.length; i++) {
-        const productLi = document.createElement('li')
-        productLi.innerHTML = 
-        `
-        <p class="product-p">${array[i].name}<span title="${array[i].description}" class="product-p-description">ℹ</span></p>
-        <p class="price-p">Price: <span class="price-p-span">$${array[i].price}</span></p>
-        <button class="button-add-product">Add to cart</button>
-        `
-        productsList.append(productLi)
-        let newButtonAdd = document.querySelectorAll('.button-add-product');
-        
-        arrayH.push(productLi)
-        newButtonAdd[i].addEventListener('click', ()=> {
-            arrayH.push(productLi)
-            for ( let i = 0; i < arrayH.length; i++) {
 
-                const productLiAdded = document.createElement('li');
-                productsCartList.append(productLiAdded);
+
+
+
+
+
+
+
+
+// // This is from for let 
+
+
+// let newButtonAdd = document.querySelectorAll('.button-add-product');
     
-                if( i < arrayH.length) {
-                    productLiAdded.innerHTML = 
-                    `
-                    <p class="product-added-name">${array[i].name}<span class="product-p-description" title="${array[i].description}">ℹ</span></p>
-                    <input type="number" class="product-added-quantity" value="1">
-                    <p class="product-added-price">$${array[i].price}</p>
-                    `   
-                } else {
-                    return;
-                }
-                // Create 
-                // let newProductAddedName = document.createElement('p')
-                // newProductAddedName.classList.add('product-added-name')
-                // newProductAddedName.textContent = `${array[i].name}`
-            
-                // let newProductAddedDescription = document.createElement('span')
-                // newProductAddedDescription.classList.add('product-p-description')
-                // newProductAddedDescription.setAttribute('title', `${array[i].description}`)
-                // newProductAddedDescription.textContent = 'ℹ'
-    
-                // let newProductAddedInput = document.createElement('input');
-                // newProductAddedInput.classList.add('product-added-quantity');
-                // newProductAddedInput.setAttribute('type', 'number')
-                // newProductAddedInput.setAttribute('value', '1')
-    
-                // let newProductAddedPrice = document.createElement('p');
-                // newProductAddedPrice.classList.add('product-added-price')
-                // newProductAddedPrice.textContent = `$${array[i].price}`
-    
-                // newProductAddedName.append(newProductAddedDescription)
-                // productLiAdded.append(newProductAddedName, newProductAddedInput, newProductAddedPrice)
-                sumTotalPrice += array[i].price;
-                totalPrice.textContent = `Total price: $${sumTotalPrice.toFixed(2)}`;
-            }
-        })
-    }
-}
-createLi(arraySortByPrice);
+// newButtonAdd[i].addEventListener('click', ()=> {
+//     const productLiAdded = document.createElement('li');
+//     productsCartList.append(productLiAdded);
 
+//     productLiAdded.innerHTML = 
+//     `
+//     <p class="product-added-name">${arraySortByPrice[i].name}<span class="product-p-description" title="${arraySortByPrice[i].description}">ℹ</span></p>
+//     <input type="number" class="product-added-quantity" value="1">
+//     <p class="product-added-price">$${arraySortByPrice[i].price}</p>
+//     `   
+//     // Create 
+//     // let newProductAddedName = document.createElement('p')
+//     // newProductAddedName.classList.add('product-added-name')
+//     // newProductAddedName.textContent = `${array[i].name}`
 
+//     // let newProductAddedDescription = document.createElement('span')
+//     // newProductAddedDescription.classList.add('product-p-description')
+//     // newProductAddedDescription.setAttribute('title', `${array[i].description}`)
+//     // newProductAddedDescription.textContent = 'ℹ'
 
+//     // let newProductAddedInput = document.createElement('input');
+//     // newProductAddedInput.classList.add('product-added-quantity');
+//     // newProductAddedInput.setAttribute('type', 'number')
+//     // newProductAddedInput.setAttribute('value', '1')
 
+//     // let newProductAddedPrice = document.createElement('p');
+//     // newProductAddedPrice.classList.add('product-added-price')
+//     // newProductAddedPrice.textContent = `$${array[i].price}`
+
+//     // newProductAddedName.append(newProductAddedDescription)
+//     // productLiAdded.append(newProductAddedName, newProductAddedInput, newProductAddedPrice)
+//     sumTotalPrice += arraySortByPrice[i].price;
+//     totalPrice.textContent = `Total price: $${sumTotalPrice.toFixed(2)}`;
+// })
